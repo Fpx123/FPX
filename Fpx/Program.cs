464f197aed191c292410bd504e62d7706cf79a51
Dell <Dell@DESKTOP-E6FB1V3>
2019-11-15 10:08:21 +0700

    Nguyễn Minh Kha Nguyễn Đức Thịnh Phạm Trần Duy Linh Nhóm:FPX

---
+++ Fpx/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fpx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bài tập 1- nhóm FPX
+            //khởi tạo project
+        }
+    }
+}
